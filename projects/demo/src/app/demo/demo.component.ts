f1d92017ea75621e3bc8f2341534df0c570953a4
c2erichsu <c2erichsu@gmail.com>
2023-09-18 23:34:44 +0800

    (demo) Update demo application's content.

--- projects/demo/src/app/demo/demo.component.ts
+++ projects/demo/src/app/demo/demo.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, NgForm, NgModel } from '@angular/forms';
 import { NgxCurrencyMaskConfig, NgxCurrencyMaskDirective, NgxCurrencyMaskService } from 'ngx-currency-mask';
 import { MarkdownService } from 'ngx-markdown';
 
@@ -7,33 +8,48 @@ import { MarkdownService } from 'ngx-markdown';
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
-export class HomeComponent implements OnInit {
-  @ViewChild('demoInput') demo1Input!: ElementRef;
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('demo1Form') demo1Form!: NgForm;
+
+  @ViewChild('demo1Input') demo1Input!: NgModel;
   static config: NgxCurrencyMaskConfig = new NgxCurrencyMaskConfig();
 
-  updControl:boolean = true;
+  updControl: boolean = true;
 
   demo1: number | string = 1000;
   demo1Locale: string = 'en-US';
   demo1Scale: string = '1.2-2';
+  demo1Max: number | string = 10000;
+  demo1Min: number | string = 10;
+  demo1Update: boolean = true;
 
   demo21 = 1000;
   demo22 = 2000;
 
-  demo1Code: string = 
-  `<input currencyMask locale="${this.demo1Locale}" scale="${this.demo1Scale}" [(ngModel)]="demo1">`;
+  demo1Code: string =
+    `<input currencyMask locale="${this.demo1Locale}" scale="${this.demo1Scale}" max="${this.demo1Max}" min="${this.demo1Min}" [(ngModel)]="demo1">`;
 
   constructor(
     public currencyMaskService: NgxCurrencyMaskService,
     private markdownService: MarkdownService,
   ) { }
 
+  ngAfterViewInit(): void {
+    this.demo1Form.valueChanges?.subscribe((res) => {
+      this.demo1Locale = res.demo1Locale;
+      this.demo1Scale = res.demo1Scale;
+      this.demo1Max = res.demo1Max;
+      this.demo1Min = res.demo1Min;
+      this.demo1Code = `<input currencyMask locale="${this.demo1Locale}" scale="${this.demo1Scale}" max="${this.demo1Max}" min="${this.demo1Min}" [(ngModel)]="demo1">`;
+      this.markdownService.reload();
+      this.demo1Update = !this.demo1Update;
+    });
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-  updateDemo1(event: any) {
-    this.demo1 = '';
-    this.demo1Code = `<input currencyMask locale="${this.demo1Locale}" scale="${this.demo1Scale}" [(ngModel)]="demo1">`;
-    this.updControl = !this.updControl;
+  show() {
+    console.log(this.demo1Input)
   }
 }
--- projects/demo/src/app/docs/docs-markdown/docs.md
+++ projects/demo/src/app/docs/docs-markdown/docs.md
@@ -1,6 +1,6 @@
-# ngx-currency-mask
+<!-- # ngx-currency-mask -->
 
-## Getting Strarted & Installation
+## Installation
 
 To add this ngx-currency-mask library to your project, use the following commands.
 
@@ -44,12 +44,22 @@ you must import `NgxCurrencyMaskModule` inside your **target Module**, and confi
   })
 ```
 
+## Config Properties
+
+This table lists the properties for `NGX_CURRENCY_MASK_CONFIG`, which can be used to configure global settings.:
+| Property | Default | Description |
+| --- | --- | --- |
+| locale: string | 'en-US' |The locale for `currencyMask` configuration. |
+| scale: string | '1.2-2' | The scale of decimal representation. |
+| textAlign: string | 'right' | The option to control `<input>` text's alignment. |
+
 ## Attributes
 
-This section lists the attributes available to `currencyMask` directive.
+This section lists the attributes available for the `currencyMask` directive. </br>
+`currencyMask` can also be adapted to work with native `<input type="text">` validator, which can refer to [MDN&lt;input&gt;Docs](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/input#%E5%B1%9E%E6%80%A7).
 
-| Property | Description |
+| Attributes | Description |
 | --- | --- |
-| @Input() scale: string | Set decimal representation, parameter written in the following format: <br> `"1.minFractionDigits-maxFractionDigits"` <br>Example: `<input currencyMask scale="1.2-2">`, would format to 9,999.99 |
-| @Input() max: number \| string | The maximum (numeric) value for this item, if exceed the maximum value, `<input>` would be invalid. |
-| @Input() min: number \| string | The minimum (numeric) value for this item, if less than minimum value, `<input>` would be invalid. |
+| @Input() **scale**: string | Set decimal representation, parameter written in the following format: </br>`"1.minFractionDigits-maxFractionDigits"` </br> Example: `<input currencyMask scale="1.2-2">`, would format to `9,999.99` |
+| @Input() **max**: string | The maximum (numeric) value for this item, if exceed the maximum value, `<input>` would be invalid. |
+| @Input() **min**: string | The minimum (numeric) value for this item, if less than minimum value, `<input>` would be invalid. |
